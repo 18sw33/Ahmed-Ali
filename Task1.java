11978d5d140edbbb2da3c0ff1677039a47da4167
18sw33 <49402563+18sw33@users.noreply.github.com>
2019-04-08 23:27:13 +0500

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,12 @@
+class Task2{
+public static void main(String args[]){
+double d = 50.25;
+long l = (long) d;
+double c = d - l;
+System.out.println("Actual Value " + d);
+System.out.println("Integral Value " + l);
+System.out.println("Fractional Value " + c);
+}
+}
+
+
