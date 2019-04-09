a91fc19ae9da14b27f6616af363d443b4cedb9d6
18sw33 <49402563+18sw33@users.noreply.github.com>
2019-04-09 16:27:38 +0500

    Add files via upload

---
+++ Lab 1/Task2.java
@@ -0,0 +1,14 @@
+class Task2{
+public static void main(String args[]){
+String name = "Ahmed Ali";
+int marks = 821;
+float percentage = 74.63;
+System.out.println("Name " + name);
+System.out.println("Marks " + marks);
+System.out.println("Percentage " + percentage);
+if(marks>=770)
+System.out.println("Grade A");
+}
+}
+
+
